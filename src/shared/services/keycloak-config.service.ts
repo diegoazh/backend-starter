a35f3b0a2ecd632eab750dc15a8987c88305bb82
Diego A. Zapata Häntsch <diegoazh2003@gmail.com>
2023-03-26 17:23:28 -0300

    fix: update keycloak config on keycloak-config-service

--- src/shared/services/keycloak-config.service.ts
+++ src/shared/services/keycloak-config.service.ts
@@ -22,6 +22,9 @@ export class KeycloakConfigService implements KeycloakConnectOptionsFactory {
       secret: this.nodeConfig.config.get('keycloak.clientSecret'),
       policyEnforcement: PolicyEnforcementMode.PERMISSIVE,
       tokenValidation: TokenValidation.ONLINE,
+      bearerOnly: true,
+      realmPublicKey:
+        'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA5Tnl61zap9uoGVwfMz738y48PXssG3DCSc4+b+gb53cWe5dTm5ciphJDy9N2QeGjOsKPC7SPF2aJXQMO6iPnc/J2reQ9KS5zH90s/tShLF136D/vMQkUBv771KWv1pEc4ztO2DAzRV0Qhxj5Jzu3R8Ff4OGQ1SFWwq3PrjMmaZZnQyxv93xuOBlFeQiZWNBHo7Xh0AkVaWexpMBNVoGqv+wyYZS6UeSEkx/0oGdojA113Zo2hiagthOGGXvx/RBtVBzefBa/03tt3P+Oo/MZ0deegM1tSklmdZx6GT/rGam9IamDsFe76WJVvTrHBXA0Cd0ggG4BeWR3zB6fK/3rxQIDAQAB',
     };
   }
 }
