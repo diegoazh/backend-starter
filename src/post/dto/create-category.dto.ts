a7352d07db58f1fea4bc80fd4af3404cf11b6d84
Diego A. Zapata Häntsch <diegoazh2003@gmail.com>
2023-06-29 11:43:52 -0300

    fix: add case validations on dtos

--- src/post/dto/create-category.dto.ts
+++ src/post/dto/create-category.dto.ts
@@ -1,8 +1,15 @@
-import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+import {
+  IsLowercase,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateCategoryDto {
   @MaxLength(200)
   @MinLength(5)
+  @IsLowercase()
   @IsString()
   @IsNotEmpty()
   name: string;
--- src/post/dto/create-tag.dto.ts
+++ src/post/dto/create-tag.dto.ts
@@ -1,8 +1,15 @@
-import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+import {
+  IsLowercase,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateTagDto {
   @MaxLength(200)
   @MinLength(3)
+  @IsLowercase()
   @IsString()
   @IsNotEmpty()
   name: string;
